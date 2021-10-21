f2616b71765af856482b8659473c41a73aa87960
Harsh-vardhan1234 <88927683+Harsh-vardhan1234@users.noreply.github.com>
2021-10-21 14:42:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 function getDetails(place){
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById('name').innerHTML = abc.name)
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById('country').innerHTML = abc.sys.country)
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById('temp').innerHTML = parseInt(abc.main.temp-273.15)+"°C")
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById('maxtemp').innerHTML = parseInt(abc.main.temp_max-273.15)+"°C")
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById('mintemp').innerHTML = parseInt(abc.main.temp_min-273.15)+"°C")
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById('lat').innerHTML = abc.coord.lat)
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById('lon').innerHTML = abc.coord.lon)
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById('visibility').innerHTML = abc.visibility)
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById('pressure').innerHTML = abc.main.pressure)
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById('humidity').innerHTML = abc.main.humidity)
 }
